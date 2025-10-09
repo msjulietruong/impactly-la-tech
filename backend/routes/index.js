2f272f857b350ad8c4a55348215eb3932fc5002b
zlaud <laudkimberly@gmail.com>
2025-10-08 20:54:35 -0700

    health

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,20 +1,28 @@
-import express from 'express';
+import express from "express";
 
-import healthController from '../controllers/healthController.js';
-import lookupController from '../controllers/lookupController.js';
-import companyController from '../controllers/companyController.js';
-import scoreController from '../controllers/scoreController.js';
+import getHealth from "../controllers/healthController.js";
+import lookupController from "../controllers/lookupController.js";
+import companyController from "../controllers/companyController.js";
+import scoreController from "../controllers/scoreController.js";
 
-import validateObjectId from '../middleware/validateObjectId.js';
+import validateObjectId from "../middleware/validateObjectId.js";
 
 const router = new express.Router();
 
-router.get('/health', healthController.getHealth);
+router.get("/health", getHealth);
 
 // NOTE(liam): v1
-router.get('/v1/lookup', lookupController.lookupProduct);
-router.get('/v1/company', companyController.getCompany);
-router.get('/v1/company/:id', validateObjectId('id'), companyController.getCompany);
-router.get('/v1/score/:companyId', validateObjectId('companyId'), scoreController.getScore);
+router.get("/v1/lookup", lookupController.lookupProduct);
+router.get("/v1/company", companyController.getCompany);
+router.get(
+  "/v1/company/:id",
+  validateObjectId("id"),
+  companyController.getCompany
+);
+router.get(
+  "/v1/score/:companyId",
+  validateObjectId("companyId"),
+  scoreController.getScore
+);
 
 export default router;
