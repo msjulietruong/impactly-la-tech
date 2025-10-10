14dc64a849469bc50989422c6c51a6f210f3ddb6
Liam Bagabag <lbscout1@gmail.com>
2025-10-09 22:05:29 -0700

    setup terraform with azure

---
+++ terraform/outputs.tf
@@ -0,0 +1,8 @@
+output "backend_url" {
+    value = azurerm_linux_web_app.backend.default_hostname
+}
+
+output "frontend_token" {
+    value = azurerm_static_web_app.frontend.api_key
+    sensitive = true
+}
