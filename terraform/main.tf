c6183b9cff356bbab11eb100798295c1e8b976dd
Liam Bagabag <lbscout1@gmail.com>
2025-10-10 21:28:58 -0700

    separated resource group of backend and frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,21 +24,27 @@ resource "azurerm_resource_group" "project" {
   location = "West US 2"
 }
 
+resource "azurerm_resource_group" "project2" {
+  name = "impactly-backend-project"
+  location = "North Central US"
+}
+
 resource "azurerm_service_plan" "backend_plan" {
   name                = "impactly-backend-plan"
-  resource_group_name = azurerm_resource_group.project.name
-  location            = azurerm_resource_group.project.location
+  resource_group_name = azurerm_resource_group.project2.name
+  location            = azurerm_resource_group.project2.location
   os_type             = "Linux"
   sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "backend" {
   name                = "impactly-backend"
-  resource_group_name = azurerm_resource_group.project.name
-  location            = azurerm_resource_group.project.location
+  resource_group_name = azurerm_resource_group.project2.name
+  location            = azurerm_resource_group.project2.location
   service_plan_id     = azurerm_service_plan.backend_plan.id
 
   site_config {
+    always_on = false
     application_stack {
       node_version = "20-lts"
     }
