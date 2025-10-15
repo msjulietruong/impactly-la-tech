5675bac1bcbde1319660a46a2369384ab9a302b2
Liam Bagabag <lbscout1@gmail.com>
2025-10-14 18:54:27 -0700

    added cosmos entries.

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   required_version = ">= 1.1.0"
   cloud {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "impactly-la-lech"
     workspaces {
       name = "impactly"
@@ -28,6 +28,63 @@ resource "azurerm_resource_group" "project" {
 resource "azurerm_resource_group" "project2" {
   name     = "impactly-backend-project"
   location = "North Central US"
+}
+
+resource "azurerm_resource_group" "project3" {
+  name     = "impactly-db-project"
+  location = "East US"
+}
+
+resource "azurerm_cosmosdb_account" "db_account" {
+  name                = "impactly-db-account"
+  location            = azurerm_resource_group.project3.location
+  resource_group_name = azurerm_resource_group.project3.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  automatic_failover_enabled = false
+  free_tier_enabled = true
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  consistency_policy {
+    consistency_level = "Session"
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.project3.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name                = "impactly-db"
+  resource_group_name = azurerm_resource_group.project3.name
+  account_name        = azurerm_cosmosdb_account.db_account.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "container" {
+  name                = "items"
+  resource_group_name = azurerm_resource_group.project3.name
+  account_name        = azurerm_cosmosdb_account.db_account.name
+  database_name       = azurerm_cosmosdb_sql_database.db.name
+  partition_key_paths  = [ "/category" ]
+
+  indexing_policy {
+    indexing_mode = "consistent"
+    included_path {
+      path = "/*"
+    }
+    excluded_path {
+      path = "/\"_etag\"/?"
+    }
+  }
+
+  unique_key {
+    paths = ["/id"]
+  }
 }
 
 resource "azurerm_service_plan" "backend_plan" {
@@ -52,7 +109,7 @@ resource "azurerm_linux_web_app" "backend" {
   }
 
   app_settings = {
-    "MONGODB_URI" = var.mongodb_uri
+    "MONGODB_URI"   = var.mongodb_uri
     "WEBSITES_PORT" = "3000"
   }
 }
